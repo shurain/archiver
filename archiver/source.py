fe8f5d88364f2ec99256c363193aa612cc413370
Sungjoo Ha <shurain@gmail.com>
2013-04-26 02:36:51 +0900

    Fetch Pinboard bookmarks from given date string until now

--- archiver/source.py
+++ archiver/source.py
@@ -4,6 +4,8 @@ except ImportError:
     # Python 2.5
     import xml.etree.cElementTree as etree
 import requests
+from datetime import datetime
+
 from item import PinboardItem
 
 
@@ -11,16 +13,22 @@ class Source(object):
     pass
 
 class PinboardSource(Source):
+    """Pinboard (https://pinboard.in/) data source.
+    Fetches bookmarks from Pinboard.
+    """
+
     URL = "https://api.pinboard.in/v1/"
 
     def __init__(self, token):
         self.token = token
 
     def grab_xml(self, url):
+        """Given URL string, fetch the content and return it."""
         r = requests.get(url)
         return r.content
 
     def parse_xml(self, xml):
+        """Given XML string, parse it and return a list of PinboardItem."""
         root = etree.fromstring(xml)
         item_list = []
         for post in root.iterchildren():
@@ -35,9 +43,14 @@ class PinboardSource(Source):
 
         return item_list
 
-
     def fetch_from_date(self, datestr):
-        pass
+        """Fetch the bookmarks after the given date string.
+        UTC timestamp is used for date string.
+
+        datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        """
+        url = self.URL + 'posts/all?auth_token={}&fromdt={}'.format(self.token, datestr)
+        return self.parse_xml(self.grab_xml(url))
 
     def fetch_all(self):
         pass
