3e46662b6fb4b1342ab293a54ee37964ddd44ae5
Sungjoo Ha <shurain@gmail.com>
2013-04-27 16:08:53 +0900

    Adding utf-8 file option to all .py files

--- archiver/enml.py
+++ archiver/enml.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """
 HTML to ENML Converter
 
--- test_archiver.py
+++ test_archiver.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import unittest
 import mock
 from datetime import datetime
